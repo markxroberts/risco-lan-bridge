09c8af448bd30b682a6d61ac6c52b78b240efdc3
Mark Roberts <mark@markroberts.org.uk>
2023-04-07 12:37:44 +0100

    declare output.status

--- src/Devices/Outputs.ts
+++ src/Devices/Outputs.ts
@@ -70,7 +70,7 @@ export class Output extends EventEmitter {
 
     this.Type = Type || 0
     this.OStatus = OStatus || '--'
-    this.status = OStatus
+    this.status = OStatus || ''
 
     this.PulseDelay = 0
     this.FirstStatus = true
