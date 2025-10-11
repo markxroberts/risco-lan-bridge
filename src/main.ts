fd2ee3cc8a05f9e9e031735dc849e781b0cc0994
Mark Roberts <mark@markroberts.org.uk>
2025-10-11 15:16:53 +0100

    don't convert to json

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,13 @@ import yaml from 'js-yaml';
 function readConfig(): PanelOptions {
   const configPathjson = path.join(process.cwd(), 'config.json');
   const configPathyaml = path.join(process.cwd(), 'config.yaml');
-//  if (fs.existsSync(configPathjson)) {
-//    console.log('Loading config from: ' + configPathjson);
-//    return JSON.parse(readFileSync(configPathjson, 'utf-8'));
-//  }
-  if (fs.existsSync(configPathyaml)) {
+  if (fs.existsSync(configPathjson)) {
+    console.log('Loading config from: ' + configPathjson);
+    return JSON.parse(readFileSync(configPathjson, 'utf-8'));
+  }
+  else if (fs.existsSync(configPathyaml)) {
     console.log('Loading config from: ' + configPathyaml);
-    return JSON.parse(yaml.load(readFileSync(configPathyaml, 'utf-8')));
+    return yaml.load(readFileSync(configPathyaml, 'utf-8'));
   } else {
     throw new Error('Config file does not exist.  Please ensure config.json or config.yaml present before restarting.');
   }
